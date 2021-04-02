c78d7878276d351dafad145febd530c835f761d5
codingmasawe <44496652+codingmasawe@users.noreply.github.com>
2021-04-02 23:28:40 +0200

    added show action and view for lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/lists", to: "lists#index"
+
+  get "lists/:id", to: "lists#show"
+  get "/lits/new", to: "lists#new"
 end
+
