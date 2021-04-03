6e240940bebd1eb104eca6f25ba6b86f3d4b44ed
codingmasawe <44496652+codingmasawe@users.noreply.github.com>
2021-04-03 13:04:33 +0200

    added create methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   get 'lists/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/lists", to: "lists#index"
-
-  get "lists/:id", to: "lists#show"
-  get "/lits/new", to: "lists#new"
+  resources :lists
 end
 
